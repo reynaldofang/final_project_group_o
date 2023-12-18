75f7bb721abf252da9c78030fd3d88e46a89704f
Ester <esteroa0310@gmail.com>
2023-12-18 22:46:33 +0700

    #4 add new endpoints for admin

--- Backend/beritaMakanan/route.js
+++ Backend/beritaMakanan/route.js
@@ -1,9 +1,13 @@
 const {Router} = require("express")
-const {allNews, newsById} = require("./controller")
+const {allNews, latestNews, newsById} = require("./controller")
+const {createNews, deleteNewsById} = require("./admin-controller")
 
 const newsRoutes = Router()
 
 newsRoutes.get("", allNews)
+newsRoutes.get("/latest", latestNews)
 newsRoutes.get("/:id", newsById)
+newsRoutes.post("", createNews)
+newsRoutes.delete("/:id", deleteNewsById)
 
 module.exports = newsRoutes
